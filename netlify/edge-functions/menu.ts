ac3738c3fc960e53aa0aa246ee299ba0ca66292a
Rafael Bardini <rafael@rbardini.com>
2022-06-06 16:44:57 +0200

    Migrate Netlify Functions to Edge Functions

--- netlify/edge-functions/menu.ts
+++ netlify/edge-functions/menu.ts
@@ -1,26 +1,49 @@
-import fetch from 'node-fetch'
-
 // https://github.com/JopiterApp/USP-Restaurant-API
 const API_ENDPOINT = 'https://uspdigital.usp.br/rucard/servicos/menu/'
-const RESTAURANT_ID = 2
+const RESTAURANT_ID = '2'
 
-export const handler = async event => {
+type Meal = {
+  menu: string
+  calories: string
+}
+
+type Day = {
+  date: string
+  lunch: Meal
+  dinner: Meal
+}
+
+type Response = {
+  message: {
+    error: boolean
+    message: string
+  }
+  meals: Day[]
+  observation: {
+    observation: string
+  }
+}
+
+export default async (req: Request) => {
   // Only allow HTTP GET method
-  if (event.httpMethod !== 'GET') {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({
+  if (req.method !== 'GET') {
+    return new Response(
+      JSON.stringify({
         message: { error: true, message: 'Method Not Allowed' },
       }),
-    }
+      {
+        headers: { 'content-type': 'application/json' },
+        status: 405,
+      },
+    )
   }
 
   try {
     const response = await fetch(new URL(RESTAURANT_ID, API_ENDPOINT), {
       method: 'POST',
-      body: new URLSearchParams({ hash: process.env.MENU_API_HASH }),
+      body: new URLSearchParams({ hash: Deno.env.get('MENU_API_HASH')! }),
     })
-    const data = await response.json()
+    const data = (await response.json()) as Response
 
     data.meals.forEach(meal => {
       const { date, lunch, dinner } = meal
@@ -30,6 +53,7 @@ export const handler = async event => {
 
       // Normalize and split dishes
       ;[lunch, dinner].forEach(meal => {
+        // @ts-expect-error converting string to string array
         meal.menu = meal.menu
           .replace('Arroz/Feijão', 'Arroz e feijão')
           .replace('Saladas: Diversas', 'Saladas diversas')
@@ -39,13 +63,18 @@ export const handler = async event => {
       })
     })
 
-    return { statusCode: 200, body: JSON.stringify(data) }
+    return new Response(JSON.stringify(data), {
+      headers: { 'content-type': 'application/json' },
+    })
   } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
+    return new Response(
+      JSON.stringify({
         message: { error: true, message: error.message },
       }),
-    }
+      {
+        headers: { 'content-type': 'application/json' },
+        status: 500,
+      },
+    )
   }
 }
